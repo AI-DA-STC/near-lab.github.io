9be410cf550e59f214110184612bf50a6e7db6f8
krishna22112023 <151725444+krishna22112023@users.noreply.github.com>
2026-01-25 06:54:59 +0000

    Deploying to gh-pages from @ AI-DA-STC/near-lab.github.io@d0f4952def283ac3e960dc2a78aee4b67902a710 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "AI-DA-STC/near-lab-website",
+    "data-repo": "AI-DA-STC/near-lab.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
