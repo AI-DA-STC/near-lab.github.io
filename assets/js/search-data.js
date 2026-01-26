fade366152afa6ea4b00961accba26f4aa46b18f
krishna22112023 <151725444+krishna22112023@users.noreply.github.com>
2026-01-26 03:02:58 +0000

    Deploying to gh-pages from @ AI-DA-STC/near-lab.github.io@5629ddfbd739d8be1382ef91e833c54aeada38f4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-next-gen-edge-ai-robotics-near-lab",
-    title: "Next-gen Edge AI & Robotics (NEAR) Lab",
+    id: "nav-about",
+    title: "About",
     section: "Navigation",
     handler: () => {
       window.location.href = "/near-lab.github.io/";
