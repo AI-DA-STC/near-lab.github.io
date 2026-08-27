f2f57ae3b2c50597a12e777ca4a377f9c6c96ad0
williamteo-st <151591651+williamteo-st@users.noreply.github.com>
2026-08-27 00:19:48 +0000

    Deploying to gh-pages from @ AI-DA-STC/near-lab.github.io@ef47369c3642b1a2cf7b6e5c891034818c270205 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4,13 +4,13 @@ const ninja = document.querySelector('ninja-keys');
 // add the home and posts menu items
 ninja.data = [{
     id: "nav-about",
-    title: "About",
+    title: "about",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
     },
   },{id: "nav-research",
-          title: "Research",
+          title: "research",
           description: "Robust Physical AI for autonomous robotic systems operating in real-world, resource-constrained environments.",
           section: "Navigation",
           handler: () => {
